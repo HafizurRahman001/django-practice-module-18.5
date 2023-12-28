19048a609d9b04fcce95334110cc003b7b9ce279
hafizur rahman <hafizurrahmanbu@gmail.com>
2023-12-29 01:57:09 +0600

    hide toast feature added

--- authentication_project/authentication_project/views.py
+++ authentication_project/authentication_project/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
-@login_required(login_url="/authentication/user_login/")
+
 def home(request):
     return render(request,'base.html')
